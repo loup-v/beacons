d84ded63330227aad72da223957117db155352ae
Aawaz Gyawali <awazgyawali@gmail.com>
2019-03-09 22:54:34 +0545

    Fixed example bug

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,13 +5,6 @@ import 'package:beacons/beacons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:flutter_local_notifications/initialization_settings.dart';
-import 'package:flutter_local_notifications/notification_details.dart';
-import 'package:flutter_local_notifications/platform_specifics/android/initialization_settings_android.dart';
-import 'package:flutter_local_notifications/platform_specifics/android/notification_details_android.dart';
-import 'package:flutter_local_notifications/platform_specifics/ios/initialization_settings_ios.dart';
-import 'package:flutter_local_notifications/platform_specifics/ios/notification_details_ios.dart';
-
 import 'tab_monitoring.dart';
 import 'tab_ranging.dart';
 
@@ -26,20 +19,20 @@ class MyApp extends StatefulWidget {
     Beacons.backgroundMonitoringEvents().listen((event) {
       FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
           new FlutterLocalNotificationsPlugin();
-      InitializationSettingsAndroid initializationSettingsAndroid =
-          new InitializationSettingsAndroid('app_icon');
-      InitializationSettingsIOS initializationSettingsIOS =
-          new InitializationSettingsIOS();
+      AndroidInitializationSettings initializationSettingsAndroid =
+          new AndroidInitializationSettings('app_icon');
+      IOSInitializationSettings initializationSettingsIOS =
+          new IOSInitializationSettings();
       InitializationSettings initializationSettings =
           new InitializationSettings(
               initializationSettingsAndroid, initializationSettingsIOS);
       flutterLocalNotificationsPlugin.initialize(initializationSettings);
 
-      NotificationDetailsAndroid androidPlatformChannelSpecifics =
-          new NotificationDetailsAndroid('your channel id', 'your channel name',
+      AndroidNotificationDetails androidPlatformChannelSpecifics =
+          new AndroidNotificationDetails('your channel id', 'your channel name',
               'your channel description');
-      NotificationDetailsIOS iOSPlatformChannelSpecifics =
-          new NotificationDetailsIOS();
+      IOSNotificationDetails iOSPlatformChannelSpecifics =
+          new IOSNotificationDetails();
       NotificationDetails platformChannelSpecifics = new NotificationDetails(
           androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
       flutterLocalNotificationsPlugin.show(
