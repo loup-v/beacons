576f5208dddde8c9006d8387e6997b1cf400a997
Aawaz Gyawali <awazgyawali@gmail.com>
2019-12-04 13:00:23 +0545

    Added service id to scan beacons from snowm only

--- example/ios/Runner/GeneratedPluginRegistrant.m
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -3,9 +3,24 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<beacons/BeaconsPlugin.h>)
 #import <beacons/BeaconsPlugin.h>
+#else
+@import beacons;
+#endif
+
+#if __has_include(<flutter_local_notifications/FlutterLocalNotificationsPlugin.h>)
 #import <flutter_local_notifications/FlutterLocalNotificationsPlugin.h>
+#else
+@import flutter_local_notifications;
+#endif
+
+#if __has_include(<streams_channel/StreamsChannelPlugin.h>)
 #import <streams_channel/StreamsChannelPlugin.h>
+#else
+@import streams_channel;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
