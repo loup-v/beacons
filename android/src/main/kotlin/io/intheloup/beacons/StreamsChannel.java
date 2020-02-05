b9a3f4fb3fd32563fd2fa964ccce871d6b702ed5
Brainants PC <pc.brainants@gmail.com>
2020-02-05 22:39:17 +0545

    Fixed import

--- android/src/main/kotlin/io/intheloup/beacons/StreamsChannel.java
+++ android/src/main/kotlin/io/intheloup/beacons/StreamsChannel.java
@@ -1,10 +1,5 @@
 package io.intheloup.beacons;
 
-//  Copyright (c) 2018 Loup Inc.
-//  Licensed under Apache License v2.0
-
-package io.intheloup.beacons;
-
 import android.annotation.SuppressLint;
 import android.util.Log;
 
